f18c84a4dca49fb71b95769cf88618f177238dc3
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2024-10-11 01:54:57 -0500

    Time: 43 ms (85.27%), Space: 16.6 MB (61.01%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -1,10 +1,9 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        if len(moves)%2==1:
-            return False
+        
         dist=Counter(moves)
-        if dist['L']==dist['R'] and dist['U']==dist['D']:
-            return True
-        else:
-            return False
+        return dist['L']==dist['R'] and dist['U']==dist['D']
+        #     return True
+        # else:
+        #     return False
         
