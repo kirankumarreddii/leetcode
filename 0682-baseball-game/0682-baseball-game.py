9b4ee95d8be66ee32088f950f0fffea17880f25c
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2024-10-16 13:01:39 -0500

    Time: 42 ms (68.04%), Space: 16.8 MB (14.51%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -3,14 +3,12 @@ class Solution:
         res=[]
         for op in operations:
             if op=='+':
-                res.append(res[-2]+res[-1])
-                
+                res.append(res[-2]+res[-1])    
             elif op=='D':
                 res.append(2*res[-1])
                 print(res)
             elif op=='C':
                 res.pop()
-                print(res)
             else:
                 res.append(int(op))
         return sum(res)
