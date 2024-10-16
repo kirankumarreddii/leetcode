cffed3b135772eb0b998c81403c12038ca57891b
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2024-10-16 13:02:11 -0500

    Time: 48 ms (27.84%), Space: 16.7 MB (81.89%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -6,7 +6,6 @@ class Solution:
                 res.pop()    
             elif op=='D':
                 res.append(2*res[-1])
-                print(res)
             elif op=='+':
                 res.append(res[-2]+res[-1])    
             else:
