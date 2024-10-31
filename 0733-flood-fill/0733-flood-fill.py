9d2ec6f375c5b7bb0c7c1eaffd4c3950eba2b377
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2024-10-31 14:16:37 -0500

    Time: 0 ms (100%), Space: 16.8 MB (58.59%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -2,21 +2,18 @@ class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         same = image[sr][sc]
         r, c = len(image), len(image[0])
-        
+        stack=[(sr,sc)]
         # If the starting pixel already has the target color, no need to proceed
         if same == color:
             return image
 
-        def paint(i,j):
-            if i<0 or i>=r or j<0 or j>=c or image[i][j]!= same:
-                return
-            image[i][j]=color
-            paint(i+1,j)
-            paint(i-1,j)
-            paint(i,j+1)
-            paint(i,j-1)
-
-
-
-        paint(sr,sc)
+        while stack:
+            i,j=stack.pop()
+            if 0<=i<r and 0<=j<c and image[i][j]==same:
+                image[i][j]=color
+                stack.append((i+1,j))
+                stack.append((i-1,j))
+                stack.append((i,j+1))
+                stack.append((i,j-1))
+        
         return image
