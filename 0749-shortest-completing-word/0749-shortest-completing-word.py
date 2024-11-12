de533ccfd5621dc89e76fb3232f9357b74ae0307
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2024-11-12 12:37:55 -0600

    Time: 35 ms (12.3%), Space: 17 MB (41.79%) - LeetHub

---
+++ 0749-shortest-completing-word/0749-shortest-completing-word.py
@@ -0,0 +1,15 @@
+class Solution:
+    def shortestCompletingWord(self, licensePlate: str, words: List[str]) -> str:
+        st=''.join([char.lower() for char in licensePlate if char.isalpha()])
+        count_st=Counter(st)
+        result=''
+        for word in words:
+            word_count=Counter(word.lower())
+
+            if all(word_count[char]>=count_st[char] for char in count_st):
+                if result == '' or len(result)>len(word):
+                    result=word
+        return result
+
+                
+
