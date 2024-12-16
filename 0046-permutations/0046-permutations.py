047dd4d1907d8d99fe34deeba22a8258e1b126e9
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2024-12-15 22:09:34 -0600

    Time: 0 ms (100%), Space: 17.4 MB (16.37%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,37 +1,17 @@
-# class Solution:
-#     def permute(self, nums: List[int]) -> List[List[int]]:
-#         res=[]
-#         def track(used,path):
-#             if path==len(nums):
-#                 res.append(path[:])
-#                 return
-#             for i in range(len(nums)):
-#                 if not used[i]:
-#                     path.append(nums[i])
-#                     used[i]=True
-#                     track(used,path)
-
-#                     used[i]=False
-#                     path.pop()
-#             track([False]*len(nums),[])
-#             return res
-
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        
-        def track(used, path):
-            if len(path) == len(nums):
+        res=[]
+        def track(used,path):
+            if len(path)==len(nums):
                 res.append(path[:])
                 return
-            
             for i in range(len(nums)):
                 if not used[i]:
                     path.append(nums[i])
-                    used[i] = True
-                    track(used, path)
-                    used[i] = False
+                    used[i]=True
+                    track(used,path)
+
+                    used[i]=False
                     path.pop()
-        
-        track([False] * len(nums), [])
+        track([False]*len(nums),[])
         return res
