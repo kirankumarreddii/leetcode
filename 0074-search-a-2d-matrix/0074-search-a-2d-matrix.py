d6d86ae9a67a92d0f0a1e340a0994b7c9c7a8a0f
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2024-12-24 01:37:32 -0600

    Time: 0 ms (100%), Space: 18.2 MB (5.63%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -3,22 +3,33 @@ class Solution:
         row=len(matrix)
         col=len(matrix[0])
 
-        def bin(r):
-            left=0
-            right=col-1
-            while left<=right:
-                mid=(left+right)//2
-                if matrix[r][mid]==target:
-                    return True
-                elif matrix[r][mid]<target:
-                    left=mid+1
-                else:
-                    right=mid-1
-            return False
-        for i in range(row):
-                if matrix[i][col-1]>=target:
-                    if bin(i):
-                        return True
-                    break         
+        # def bin(r):
+        #     left=0
+        #     right=col-1
+        #     while left<=right:
+        #         mid=(left+right)//2
+        #         if matrix[r][mid]==target:
+        #             return True
+        #         elif matrix[r][mid]<target:
+        #             left=mid+1
+        #         else:
+        #             right=mid-1
+        #     return False
+        # for i in range(row):
+        #         if matrix[i][col-1]>=target:
+        #             if bin(i):
+        #                 return True
+        #             break         
+        # return False
+        left,right=0,row*col-1
+        while left<=right:
+            mid=(left+right)//2
+            midval=matrix[mid//col][mid%col]
+            if midval==target:
+                return True
+            elif midval<target:
+                left=mid+1
+            else:
+                right=mid-1
         return False
-                
+            
