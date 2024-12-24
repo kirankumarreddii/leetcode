dd6e75009fc0819f30bb65d9441db5d4d3fe3e22
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2024-12-24 15:48:54 -0600

    Time: 0 ms (100%), Space: 17.8 MB (5.57%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,26 +3,35 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        count0=0
-        count1=0
-        count2=0
-        n=len(nums)
-        for i in range(n):
-            if nums[i]==0:
-                count0+=1
-            elif nums[i]==1:
-                count1+=1
+        # count0=0
+        # count1=0
+        # count2=0
+        # n=len(nums)
+        # for i in range(n):
+        #     if nums[i]==0:
+        #         count0+=1
+        #     elif nums[i]==1:
+        #         count1+=1
+        #     else:
+        #         count2+=1
+        # for i in range(0,count0):
+        #     # print(i)
+        #     nums[i]=0
+        # for i in range(count0,count1+count0):
+        #     print(i)
+        #     nums[i]=1
+        # for i in range(count1+count0,n):
+        #     print(i)
+        #     nums[i]=2
+        low,mid,high=0,0,len(nums)-1
+        while mid<=high:
+            if nums[mid]==0:
+                nums[low],nums[mid]=nums[mid],nums[low]
+                mid+=1
+                low+=1
+            elif nums[mid]==2:
+                nums[high],nums[mid]=nums[mid],nums[high]
+                high-=1
             else:
-                count2+=1
-        for i in range(0,count0):
-            # print(i)
-            nums[i]=0
-        for i in range(count0,count1+count0):
-            print(i)
-            nums[i]=1
-        for i in range(count1+count0,n):
-            print(i)
-            nums[i]=2
-        
-        
+                mid+=1
 
