7c26cfbe86ec4eee26549921671b967a8362c19f
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2024-12-26 02:35:46 -0600

    Time: 64 ms (5.98%), Space: 17.6 MB (11.36%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -6,28 +6,38 @@ class Solution:
             return 1
         left,right=0,1
         count=1
+        # while right<len(nums):
+        #     if nums[right]!=nums[right-1]:
+        #             if count==1:
+        #                 nums[left]=nums[right-1]
+        #                 left+=1
+        #             else:
+        #                 nums[left]=nums[right-1]
+        #                 left+=1
+        #                 nums[left]=nums[right-1]
+        #                 left+=1
+        #             count=1
+        #     else:
+        #         count+=1
+        #     right+=1
+        # if nums[-2]==nums[-1]:
+        #     nums[left]=nums[-1]
+        #     left+=1
+        #     nums[left]=nums[right-1]
+        #     left+=1
+        # else:
+        #     nums[left]=nums[right-1]
+        #     left+=1
+        # return left
+        left+=1
         while right<len(nums):
-            if nums[right]!=nums[right-1]:
-                    if count==1:
-                        nums[left]=nums[right-1]
-                        left+=1
-                    else:
-                        nums[left]=nums[right-1]
-                        left+=1
-                        nums[left]=nums[right-1]
-                        left+=1
-                    count=1
-            else:
+            if nums[right]==nums[right-1]:
                 count+=1
+            else:
+                count=1
+            if count<=2:
+                nums[left]=nums[right]
+                left+=1
             right+=1
-        if nums[-2]==nums[-1]:
-            nums[left]=nums[-1]
-            left+=1
-            nums[left]=nums[right-1]
-            left+=1
-        else:
-            nums[left]=nums[right-1]
-            left+=1
         return left
-
                 
