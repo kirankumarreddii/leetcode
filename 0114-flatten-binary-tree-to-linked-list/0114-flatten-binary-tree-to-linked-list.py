a271928b545374786a87e01922fce4cc725dcd5c
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2025-01-04 13:35:32 -0600

    Time: 0 ms (100%), Space: 18.1 MB (12.92%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
@@ -9,22 +9,40 @@ class Solution:
         """
         Do not return anything, modify root in-place instead.
         """
-        if not root:
-            return None
-        nodes=[]
-        stack=[]
-        stack.append(root)
-        while stack:
-            node=stack.pop()
-            if node.right:
-                stack.append(node.right)
-            if node.left:
-                stack.append(node.left)
-            nodes.append(node)
-        root=nodes.pop(0) 
-        for node in nodes:
-            root.right=node
-            root.left=None
-            root=root.right
-        return root
+        # if not root:
+        #     return None
+        # nodes=[]
+        # stack=[]
+        # stack.append(root)
+        # while stack:
+        #     node=stack.pop()
+        #     if node.right:
+        #         stack.append(node.right)
+        #     if node.left:
+        #         stack.append(node.left)
+        #     nodes.append(node)
+        # root=nodes.pop(0) 
+        # for node in nodes:
+        #     root.right=node
+        #     root.left=None
+        #     root=root.right
+        # return root
 
+        if not root or (not root.left and not root.right):
+            return root
+        
+        newLeft = self.flatten(root.left)
+        newRight = self.flatten(root.right)
+
+        curr = newLeft
+        if not curr:
+            return root
+        
+        while curr.right:
+            curr = curr.right
+        
+        curr.right = newRight
+        root.left = None
+        root.right = newLeft
+
+        return root
