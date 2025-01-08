3b38571442656338e0d294ce20f17b65f416b1c0
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2025-01-08 14:08:34 -0600

    Time: 43 ms (46.31%), Space: 18.2 MB (13.4%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -11,15 +11,26 @@ class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         if not node:
             return
-        visited={}
+        # visited={}
 
-        def dfs(node):
-            if node in visited:
-                return visited[node]
-            clone=Node(node.val)
-            visited[node]=clone
-            for n in node.neighbors:
-                    clone.neighbors.append(dfs(n))
-            return clone
-        return dfs(node)
-        
+        # def dfs(node):
+        #     if node in visited:
+        #         return visited[node]
+        #     clone=Node(node.val)
+        #     visited[node]=clone
+        #     for n in node.neighbors:
+        #             clone.neighbors.append(dfs(n))
+        #     return clone
+        # return dfs(node)
+        Clone={}
+        queue=deque([node])
+        Clone[node]=Node(node.val)
+
+        while queue:
+            current=queue.popleft()
+            for adj in current.neighbors:
+                if adj not in Clone:
+                    Clone[adj]=Node(adj.val)
+                    queue.append(adj)
+                Clone[current].neighbors.append(Clone[adj])
+        return Clone[node]
