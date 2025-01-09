d888236f17e96e6998f461c2de40c390732a8eda
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2025-01-09 04:19:17 -0600

    Time: 33 ms (89.49%), Space: 18.5 MB (21.51%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -11,27 +11,44 @@ class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
             return None
+        # cur=head
+        # while cur:
+        #     clone=Node(cur.val)
+        #     clone.next=cur.next
+        #     cur.next=clone
+        #     cur=cur.next.next
+        # cur=head
+        # while cur:
+        #     if cur.random:
+        #         cur.next.random=cur.random.next
+        #     cur=cur.next.next
+        # clone_head=head.next
+        # cur=head
+        # while cur:
+        #     clone=cur.next
+        #     cur.next=clone.next
+        #     cur=cur.next
+        #     if clone.next:
+        #         # clone.next=cur.next or below line same
+        #         clone.next=clone.next.next
+        # return clone_head
+        
         cur=head
+        visited={}
         while cur:
             clone=Node(cur.val)
-            clone.next=cur.next
-            cur.next=clone
-            cur=cur.next.next
-        cur=head
-        while cur:
-            if cur.random:
-                cur.next.random=cur.random.next
-            cur=cur.next.next
-        clone_head=head.next
-        cur=head
-        while cur:
-            clone=cur.next
-            cur.next=clone.next
+            visited[cur]=clone
             cur=cur.next
-            if clone.next:
-                # clone.next=clone.next.next
-                clone.next=clone.next.next
-        return clone_head
-        
+        cur=head
+        while cur:
+            if cur.next:
+                visited[cur].next=visited[cur.next]
+            if cur.random:
+                visited[cur].random=visited[cur.random]
+            cur=cur.next
+        return visited[head]
+
+
+
 
         
