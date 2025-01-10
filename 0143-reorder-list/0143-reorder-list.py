0e1bc7bc251e4bf1e7e01f53aacfaa77b959c853
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2025-01-10 00:50:50 -0600

    Time: 4 ms (38.15%), Space: 23 MB (42.01%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -1,29 +1,58 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, val=0, next=None):
+# #         self.val = val
+# #         self.next = next
+# class Solution:
+#     def reorderList(self, head: Optional[ListNode]) -> None:
+#         """
+#         Do not return anything, modify head in-place instead.
+#         """
+#         slow,fast=head,head
+#         while fast and fast.next:
+#             slow=slow.next
+#             fast=fast.next.next
+#         cur=slow
+#         prev=None
+#         while cur:
+#             temp=cur.next
+#             cur.next=prev
+#             prev=cur
+#             cur=temp
+#         first,second=head,prev
+#         while second.next:
+#             temp1=first.next
+#             temp2=second.next
+#             first.next=second
+#             second.next=temp1
+#             first=temp1  # first=second.next
+#             second=temp2
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        """
-        Do not return anything, modify head in-place instead.
-        """
-        slow,fast=head,head
+        fast = head
+        slow = head
+
+        # Step 1: Find the middle of the list
         while fast and fast.next:
-            slow=slow.next
-            fast=fast.next.next
-        cur=slow
-        prev=None
-        while cur:
-            temp=cur.next
-            cur.next=prev
-            prev=cur
-            cur=temp
-        first,second=head,prev
-        while second.next:
-            temp1=first.next
-            temp2=second.next
-            first.next=second
-            second.next=temp1
-            first=temp1  # first=second.next
-            second=temp2
+            fast = fast.next.next
+            slow = slow.next
+
+        # Step 2: Reverse the second half of the list
+        second = slow.next
+        slow.next = None
+        node = None
+
+        while second:
+            temp = second.next
+            second.next = node
+            node = second
+            second = temp
+
+        # Step 3: Merge the two halves
+        first = head
+        second = node
+
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next, second.next = second, temp1
+            first, second = temp1, temp2
