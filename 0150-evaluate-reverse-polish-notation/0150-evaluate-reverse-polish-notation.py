3b31c92c2507640d24ca44f37f73c1f1ee5d1a67
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2025-01-11 02:38:22 -0600

    Time: 2 ms (73.02%), Space: 19.2 MB (32.28%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,26 +1,40 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        stack=[int(tokens[0])]
-        i=1
-        while stack:
-            if i<len(tokens):
-                if tokens[i] not in ["+", "-", "*", "/"]:
-                    stack.append(int(tokens[i]))
-                    i+=1
-                else:
-                    right=stack.pop()
-                    left=stack.pop()
-                    if tokens[i] == "+":
-                        stack.append(left + right)
-                    elif tokens[i] == "-":
-                        stack.append(left - right)
-                    elif tokens[i] == "*":
-                        stack.append(left * right)
-                    elif tokens[i] == "/":
-                        stack.append(int(left / right))
-                    i+=1
+        # stack=[int(tokens[0])]
+        # i=1
+        # while stack:
+        #     if i<len(tokens):
+        #         if tokens[i] not in ["+", "-", "*", "/"]:
+        #             stack.append(int(tokens[i]))
+        #             i+=1
+        #         else:
+        #             right=stack.pop()
+        #             left=stack.pop()
+        #             if tokens[i] == "+":
+        #                 stack.append(left + right)
+        #             elif tokens[i] == "-":
+        #                 stack.append(left - right)
+        #             elif tokens[i] == "*":
+        #                 stack.append(left * right)
+        #             elif tokens[i] == "/":
+        #                 stack.append(int(left / right))
+        #             i+=1
+        #     else:
+        #         break
+        # return stack[0]
+        stack=[]
+        for token in  tokens:
+            if token not in ["+", "-", "*", "/"]:
+                stack.append(int(token))
             else:
-                break
+                right=stack.pop()
+                left=stack.pop()
+                if token == "+":
+                    stack.append(left + right)
+                elif token == "-":
+                    stack.append(left - right)
+                elif token == "*":
+                    stack.append(left * right)
+                elif token == "/":
+                    stack.append(int(left / right))
         return stack[0]
-
-                    
