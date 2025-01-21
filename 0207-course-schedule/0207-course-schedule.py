c331e4a580580ad8e877afeccc14ad67730ba39e
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2025-01-20 22:05:26 -0600

    Time: 3 ms (89.27%), Space: 19.4 MB (26.82%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -19,59 +19,26 @@ class Solution:
         #     return True
         # else:
         #     return False
-        # in_adj=defaultdict(list)
-        # for cour,pre in prerequisites:
-        #     in_adj[pre].append(cour)
-        # visited=[0]*numCourses
-
-        # def dfs(visited,cours):
-        #     if visited[cours]==1:
-        #         return True
-        #     if visited[cours]==2:
-        #         return False
-        #     visited[cours]=1
-        #     for c in in_adj[cours]:
-        #         if dfs(visited,c):
-        #             return False
-        #     visited[cours]=2
-        #     return False
-        # for cours in range(numCourses):
-        #     if visited[cours]==0:
-        #         if dfs(visited,cours):
-        #             return False
-        # return True
-        
-        
-           
-        in_adj = defaultdict(list)
-        for cour, pre in prerequisites:
+        in_adj=defaultdict(list)
+        for cour,pre in prerequisites:
             in_adj[pre].append(cour)
-        
-        visited = [0] * numCourses  # 0: unvisited, 1: visiting, 2: visited (processed)
-        
-        def dfs(visited, cours):
-            if visited[cours] == 1:  # Cycle detected (course is in progress)
-                return True  # Return True if a cycle is detected
-            if visited[cours] == 2:  # Already processed, no cycle
-                return False  # Return False if no cycle is detected
-            
-            # Mark the course as visiting (in progress)
-            visited[cours] = 1
-            
-            # Visit all the prerequisites (neighbors) of the course
-            for c in in_adj[cours]:  # Iterate over all courses dependent on current course
-                if dfs(visited, c):  # If a cycle is detected in the prerequisites
-                    return True  # Return True if a cycle is detected
-            
-            # Mark the course as fully processed
-            visited[cours] = 2
-            return False  # Return False if no cycle was detected in the prerequisites
-        
-        # Perform DFS for each course
+        visited=[0]*numCourses
+
+        def dfs(visited,cours):
+            if visited[cours]==1:
+                return True
+            if visited[cours]==2:
+                return False
+            visited[cours]=1
+            for c in in_adj[cours]:
+                if dfs(visited,c):
+                    return True
+            visited[cours]=2
+            return False
         for cours in range(numCourses):
-            if visited[cours] == 0:  # If the course hasn't been visited yet
-                if dfs(visited, cours):  # If a cycle is detected, return False
+            if visited[cours]==0:
+                if dfs(visited,cours):
                     return False
-        
-        # If no cycle is detected, return True
         return True
+        
+        
