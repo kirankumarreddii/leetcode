596ad67ac7b4b378fdcd83f7a72518a5601b6032
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2025-01-22 00:26:12 -0600

    Time: 33 ms (88.06%), Space: 37.2 MB (32.88%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.py
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.py
@@ -1,28 +1,12 @@
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        # if not arr:
-        #     return []
-        # ranks=[]
-        # dist={}
-        # sort_list=sorted(arr)
-        # dist[sort_list[0]]=1
-        # count=1
-        # for i in range(1,len(arr)):
-        #     if sort_list[i]>sort_list[i-1]:
-        #         count+=1
-        #     dist[sort_list[i]]=count
-        # for i in range(len(arr)):
-        #     ranks.append(dist[arr[i]])
-        # return ranks
         if not arr:
             return []
-        
-        dist = {}
-        sort_list = sorted(set(arr))  # Remove duplicates and sort
-        count = 1
-        
+        ranks=[]
+        count=0
+        dist={}
+        sort_list=sorted(set(arr))
         for i in range(len(sort_list)):
-            dist[sort_list[i]] = count
-            count += 1  # Assign increasing ranks
-        
-        return [dist[num] for num in arr]  # Map original array values to their ranks
+            count+=1
+            dist[sort_list[i]]=count
+        return [dist[num] for num in arr]
