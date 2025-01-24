6910beb1e979dca64cd86b528f5194a7102346bd
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2025-01-24 16:23:32 -0600

    Time: 0 ms (100%), Space: 18.8 MB (77.54%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,30 +1,30 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        dist={i:[] for i in range(numCourses)}
-        for pre,cour in prerequisites:
-            dist[cour].append(pre)
+        # dist={i:[] for i in range(numCourses)}
+        # for pre,cour in prerequisites:
+        #     dist[cour].append(pre)
             
-        visited=[0]*numCourses
-        res=[]
-        def dfs(i):
-            if visited[i]==1:
-                return True
-            if visited[i]==2:
-                return False
-            visited[i]=1
-            for neigh in dist[i]:
-                if dfs(neigh):
-                        return True
+        # visited=[0]*numCourses
+        # res=[]
+        # def dfs(i):
+        #     if visited[i]==1:
+        #         return True
+        #     if visited[i]==2:
+        #         return False
+        #     visited[i]=1
+        #     for neigh in dist[i]:
+        #         if dfs(neigh):
+        #                 return True
 
-            visited[i]=2
-            res.append(i)
-            return False
-        for i in range(numCourses):
-                if dfs(i):
-                    return []
-        return res[::-1]
+        #     visited[i]=2
+        #     res.append(i)
+        #     return False
+        # for i in range(numCourses):
+        #         if dfs(i):
+        #             return []
+        # return res[::-1]
 
-        
+
         dist={i:[] for i in range(numCourses)}
         course={i:0 for i in range(numCourses)}
         for pre,cour in prerequisites:
