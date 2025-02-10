208a1422a5a69d1001dfeb724325605e37931289
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2025-02-10 17:46:21 -0600

    Time: 27 ms (60.62%), Space: 21.7 MB (88.1%) - LeetHub

--- 0384-shuffle-an-array/0384-shuffle-an-array.py
+++ 0384-shuffle-an-array/0384-shuffle-an-array.py
@@ -1,51 +1,19 @@
-# class Solution:
-
-#     def __init__(self, nums: List[int]):
-#         self.nums=nums
-#         self.original=nums[:]
-
-#     def reset(self) -> List[int]:
-#         return self.original
-
-#     def shuffle(self) -> List[int]:
-#         n=len(self.nums)
-#         for i in range(n):
-#             s=random.randrange(i,n)
-#             self.nums[i],self.nums[s]=self.nums[s],self.nums[i]
-#         return self.nums
-# # Your Solution object will be instantiated and called as such:
-# # obj = Solution(nums)
-# # param_1 = obj.reset()
-# # param_2 = obj.shuffle()
 class Solution:
 
     def __init__(self, nums: List[int]):
-        self.nums = nums
-        self.index = [i for i in range(0,len(nums))]
+        self.nums=nums
+        self.original=nums[:]
 
     def reset(self) -> List[int]:
-        n = len(self.nums)
-        nums2 = [0 for _ in range(0,n) ]
-        for i in range(0,n):
-            nums2[self.index[i]] = self.nums[i]
-        self.index = [i for i in range(0,n)]
-        self.nums = nums2
-        return self.nums
+        return self.original
 
     def shuffle(self) -> List[int]:
-        n = len(self.nums)
-        nums2 = [ 0 for _ in range(0,n) ]
-        index2 = [0 for _ in range(0,n) ]
-        filled = []
-        i = 0
-        while(i < n):
-            r = randint( 0,n-1)
-            if( r not in filled):
-                filled.append(r);
-                nums2[r]  =  self.nums[i]
-                index2[r] = self.index[i] 
-                i+=1
-        self.nums = nums2 ;
-        self.index = index2 
+        n=len(self.nums)
+        for i in range(n):
+            s=random.randrange(i,n)
+            self.nums[i],self.nums[s]=self.nums[s],self.nums[i]
         return self.nums
-        
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
