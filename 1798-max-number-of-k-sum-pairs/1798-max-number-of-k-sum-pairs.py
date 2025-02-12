d492d874fa723ea72231c95ced0fadb5addd4ddc
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2025-02-12 10:55:41 -0600

    Time: 525 ms (21.76%), Space: 30.6 MB (24.92%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.py
@@ -2,18 +2,9 @@ class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
         pairs=Counter(nums)
         count=0
-        # for num in nums:
-        #     if k-num in pairs:
-        #         if pairs[k-num]>0:
-        #             count+=1
-        #             pairs[k-num]-=1
-        # return count//2
         for num in nums:
-            if pairs[num] > 0 and pairs[k - num] > 0:  # Ensure both numbers exist
-                if num == k - num and pairs[num] < 2:  # Handle case when num == k - num
-                    continue
-                count += 1
-                pairs[num] -= 1
-                pairs[k - num] -= 1  # Reduce count of complement
-
-        return count
+            if k-num in pairs:
+                if pairs[k-num]>0:
+                    count+=1
+                    pairs[k-num]-=1
+        return count//2
