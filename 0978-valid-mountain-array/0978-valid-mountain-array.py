4c1c8c2387af3b8eb77a490e7e84fbc246b7a86c
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2025-02-25 22:58:05 -0600

    Time: 147 ms (99.63%), Space: 19.2 MB (50.16%) - LeetHub

--- 0978-valid-mountain-array/0978-valid-mountain-array.py
+++ 0978-valid-mountain-array/0978-valid-mountain-array.py
@@ -3,9 +3,9 @@ class Solution:
         if len(arr)<3:
             return False
         i=0
-        j=len(arr)-1
-        while i+1<len(arr) and arr[i]<arr[i+1]:
+        l=j=len(arr)-1
+        while i+1<l+1 and arr[i]<arr[i+1]:
             i+=1
         while j-1>=0 and arr[j-1]>arr[j]:
             j-=1
-        return i == j and i != 0 and i != len(arr) - 1
+        return i == j and i != 0 and i != l
