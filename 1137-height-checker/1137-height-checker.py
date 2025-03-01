c45a24eda9a8b7cad8773e1d8315fe21843b3176
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2025-03-01 07:30:44 -0600

    Time: 4 ms (14.25%), Space: 17.9 MB (32.19%) - LeetHub

--- 1137-height-checker/1137-height-checker.py
+++ 1137-height-checker/1137-height-checker.py
@@ -1,5 +1,6 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
-        # heights_sort=sorted(heights)
-        # return sum(1 for i in range(heights) if heights[i]!=heights_sort[i])
+        heights_sort=sorted(heights)
+        return sum(1 for i in range(len(heights)) if heights[i]!=heights_sort[i])
+
         return sum(a != b for a, b in zip(heights, sorted(heights)))
