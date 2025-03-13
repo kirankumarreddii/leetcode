0f1d766dfbc5cb9c329fd39a07d138b7ab2c5e4b
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2025-03-13 11:32:26 -0500

    Time: 0 ms (100%), Space: 17.6 MB (97.95%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,15 +1,15 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         res=[]
-        def tracking(idx,path):
-            if len(path)==k and sum(path)==n:
+        def tracking(idx,path,target):
+            if len(path)==k and target==n:
                 res.append(path[:])
                 return 
-            if len(path)>k or sum(path)>n:
+            if len(path)>k or target>n:
                 return 
             for i in range(idx,10):
                 path.append(i)
-                tracking(i+1,path)
+                tracking(i+1,path,target+i)
                 path.pop()
-        tracking(1,[])
+        tracking(1,[],0)
         return res
