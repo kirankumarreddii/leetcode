c8842161d33fbc25c1439ec6ae331e3493a07bfc
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2025-03-14 12:06:53 -0500

    Time: 2 ms (80.67%), Space: 18.7 MB (99.93%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,26 +1,26 @@
-# class Solution:
-#     def majorityElement(self, nums: List[int]) -> List[int]:
-#         res=[]
-#         count1=count2=0
-#         candidate1=candidate2=None
-#         for num in nums:
-#             if count1==0:
-#                 candidate1=num
-#                 count1=1
-#             elif count2==0:
-#                 candidate2=num
-#                 count2=1
-#             elif candidate1==num:
-#                 count1+=1
-#             elif candidate2==num:
-#                 count2+=1
-#             else:
-#                 count1-=1
-#                 count2-=1
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        res=[]
+        count1=count2=0
+        candidate1=candidate2=None
+        for num in nums:
+            if candidate1==num:
+                count1+=1
+            elif candidate2==num:
+                count2+=1
+            elif  count1==0:
+                candidate1=num
+                count1=1
+            elif count2==0:
+                candidate2=num
+                count2=1
+            else:
+                count1-=1
+                count2-=1
 
         
-#         return [num for num in (candidate1,candidate2) if nums.count(num)>len(nums)//3]
-class Solution:
+        return [num for num in (candidate1,candidate2) if nums.count(num)>len(nums)//3]
+# class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         if not nums:
             return []
