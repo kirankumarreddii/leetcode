f8f7ce0beb28444d01675b07aec0e781e78ee4b3
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2025-05-26 11:35:11 -0500

    Time: 4 ms (28.94%), Space: 17.9 MB (26.79%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -3,19 +3,17 @@ class Solution:
         freq=dict()
         occur=set()
         for c in s:
-            if c not in freq:
-                freq[c]=0
-            freq[c]+=1
+            freq[c]=freq.get(c,0)+1
         stack=[]
         for char in s:
             freq[char] -= 1
-            if char in occur:
-                continue
-            while stack and ord(char)<ord(stack[-1]) and freq[stack[-1]]>0:
-                p=stack.pop()
-                occur.remove(p)
-            stack.append(char)
-            occur.add(char)
+            if char not in occur:
+               
+                while stack and ord(char)<ord(stack[-1]) and freq[stack[-1]]>0:
+                    p=stack.pop()
+                    occur.remove(p)
+                stack.append(char)
+                occur.add(char)
                        
         return ''.join(stack)
 
