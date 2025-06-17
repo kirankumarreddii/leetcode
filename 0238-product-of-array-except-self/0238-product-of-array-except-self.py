55d9578633fbc13ad72e04b80734b82804248b0f
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2025-06-17 11:44:44 -0500

    Time: 19 ms (87.42%), Space: 23.4 MB (54.36%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,27 +1,37 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prod=1
-        Zero=False
-        for num in nums:
-            if num==0:
-                if not Zero:
-                    Zero=True
-                else:
-                    for i in range(len(nums)):
-                        nums[i]=0
-                    return nums
-            else:
-                prod*=num
-        if Zero:
-            for i in range(len(nums)):
-                if nums[i]==0:
-                    nums[i]=prod
-                else:
-                    nums[i]=0
-        else:
-            for i in range(len(nums)):
-                if nums[i]==0:
-                    nums[i]=prod
-                else:
-                    nums[i]=prod//nums[i]
-        return nums
+        # prod=1
+        # Zero=False
+        # for num in nums:
+        #     if num==0:
+        #         if not Zero:
+        #             Zero=True
+        #         else:
+        #             for i in range(len(nums)):
+        #                 nums[i]=0
+        #             return nums
+        #     else:
+        #         prod*=num
+        # if Zero:
+        #     for i in range(len(nums)):
+        #         if nums[i]==0:
+        #             nums[i]=prod
+        #         else:
+        #             nums[i]=0
+        # else:
+        #     for i in range(len(nums)):
+        #         if nums[i]==0:
+        #             nums[i]=prod
+        #         else:
+        #             nums[i]=prod//nums[i]
+        # return nums
+        res=[1]*len(nums)
+        p=1
+        for i  in range(len(nums)):
+            res[i]=p
+            p*=nums[i]
+        p=1
+        for i in range(len(nums)-1,-1,-1):
+            res[i]*=p
+            p*=nums[i]
+        return res
