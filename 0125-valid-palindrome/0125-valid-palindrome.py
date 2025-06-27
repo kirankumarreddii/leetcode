715a05f7f313a20775f6b154a7a858e5b2cd2d84
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2025-06-27 01:48:52 -0500

    Time: 3 ms (98.96%), Space: 18 MB (83.25%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,19 +1,32 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s=''.join(char.lower() for char in s if char.isalnum())
-        # filtered_str = ''.join([char.lower() for char in s if char.isalnum()])
-        l=0
-        r=len(s)-1
-        if s == " ":
-            return False
-        # while(l<r):
-        #     if s[l] != s[r]:
-        #         return False
+    #     s=''.join(char.lower() for char in s if char.isalnum())
+    #     # filtered_str = ''.join([char.lower() for char in s if char.isalnum()])
+    #     l=0
+    #     r=len(s)-1
+    #     if s == " ":
+    #         return False
+    #     # while(l<r):
+    #     #     if s[l] != s[r]:
+    #     #         return False
                
-        #     r-=1
-        #     l+=1
-        # return True
-        return s==s[::-1]
+    #     #     r-=1
+    #     #     l+=1
+    #     # return True
+    #     return s==s[::-1]
 
+        left=0
+        right=len(s)-1
+        while left<=right:
+            while left<right and not s[left].isalnum():
+                left+=1
+            while left<right and not s[right].isalnum():
+                right-=1
+            if s[left].lower()!=s[right].lower():
+                return False
+            left+=1
+            right-=1
+            
+        return True
 
         
