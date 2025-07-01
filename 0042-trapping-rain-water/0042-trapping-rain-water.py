d221927d35988df37209da69ece8ae3309fff84f
kirankumarreddii <124292553+kirankumarreddii@users.noreply.github.com>
2025-07-01 12:20:56 -0500

    Time: 11 ms (72.45%), Space: 19.4 MB (38.53%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,14 +1,29 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        # res=0
+        # prefix=[0]*len(height)
+        # sufix=[0]*len(height)
+        # prefix[0]=height[0]
+        # sufix[-1]=height[-1]
+        # for i in range(1,len(height)):
+        #     prefix[i]=max(prefix[i-1],height[i])
+        # for i in range(len(height)-2,-1,-1):
+        #     sufix[i]=max(sufix[i+1],height[i])
+        # for i in range(len(height)):
+        #     res+=min(prefix[i],sufix[i])-height[i]
+        # return res
+
         res=0
-        prefix=[0]*len(height)
-        sufix=[0]*len(height)
-        prefix[0]=height[0]
-        sufix[-1]=height[-1]
-        for i in range(1,len(height)):
-            prefix[i]=max(prefix[i-1],height[i])
-        for i in range(len(height)-2,-1,-1):
-            sufix[i]=max(sufix[i+1],height[i])
-        for i in range(len(height)):
-            res+=min(prefix[i],sufix[i])-height[i]
+        l,r=0,len(height)-1
+        l_max=height[l]
+        r_max=height[r]
+        while l<r:
+            if  l_max<r_max:
+                l+=1
+                l_max=max(l_max,height[l])
+                res+=l_max-height[l]
+            else:
+                r-=1
+                r_max=max(r_max,height[r])
+                res+=r_max-height[r]
         return res
